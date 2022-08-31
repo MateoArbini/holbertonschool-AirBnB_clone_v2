e6df5bf63e9200ee9726f769d1114190270a9f5c
MateoArbini <mateoarbini1998@gmail.com>
2022-08-31 17:14:17 +0000

    task 3 with pycode ok but no parenthesis in var

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+'''
+Script that starts a Flask web application
+It must listen to '0.0.0.0'
+Port: 5000
+Routes:
+/: diplay "Hello HBNB!"
+/hbnb: diplay "HBNB"
+/c/<text>: display "C" followed by the text variable(replacing "_" with a " ")
+/python/(<text>): display Python followed by the value of text
+Use option "strict_slashes=False" in root definition
+'''
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello0():
+    '''Display Hello HBNB!'''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello1():
+    '''Display HBNB'''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def ctext(text):
+    '''Display C followed by the content of var text'''
+    cleantext = text.replace("_", " ")
+    display = "C " + cleantext
+    return display
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python", strict_slashes=False)
+def pythontext(text="is cool"):
+    '''Display Python followed by the content of var text'''
+    if text:
+        if text == "is cool":
+            return "Python is cool"
+        else:
+            cleantext = text.replace("_", " ")
+            display = "Python " + cleantext
+            return display
+    else:
+        return "Python is cool"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
