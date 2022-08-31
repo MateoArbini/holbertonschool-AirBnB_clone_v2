38361f75d24e672e695e6d3509ed222c6fb61ffd
MateoArbini <mateoarbini1998@gmail.com>
2022-08-31 15:20:41 +0000

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+Script that starts a Flask web application
+It must listen to '0.0.0.0'
+Port: 5000
+Routes:
+/: diplay "Hello HBNB!"
+/hbnb: diplay "HBNB"
+Use option "strict_slashes=False" in root definition
+'''
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello0():
+    '''Display Hello HBNB!'''
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hello1():
+    '''Display HBNB'''
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
